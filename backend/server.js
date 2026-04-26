855343ae778db9a1b5a06b5c7845504bf5188b53
ashyphrodite <rddimaano2@up.edu.ph>
2026-04-26 16:17:41 +0800

    feat: server added

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+import http from 'node:http';
+import db from './db.js';
+
+const server = http.createServer(async (req, res) => {
+    // set CORS headers so the front-end can talk to the server
+    res.setHeader('Access-Constrol-Allow-Origin', '*');
+    res.setHeader('Content-Type', 'application/json');
+
+    // /api/test/ endpoint for testing
+    if (req.url === '/api/test' && req.method === 'GET') {
+        try {
+            const [rows] = await db.execute('SELECT 1');
+            res.writeHead(200);
+            res.end(JSON.stringify({ message: "Server is running and database is connected." }))
+        } catch (err) {
+            res.writeHead(500);
+            res.end(JSON.stringify({ error: err.message }));
+        }
+    } else {
+        res.writeHead(404);
+        res.end(JSON.stringify({ message: "Route not found" }));
+    }
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`)
+});
